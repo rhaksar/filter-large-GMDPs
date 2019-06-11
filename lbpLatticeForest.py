a58855d896b991899bb4ad5f925a7267c5273e93
Ravi Haksar <rhakczar@gmail.com>
2019-06-10 18:14:05 -0700

    Updated applications to use generic LBP implementation.

--- lbpLatticeForest.py
+++ lbpLatticeForest.py
@@ -8,184 +8,193 @@ import sys
 import time
 
 sys.path.append(os.getcwd() + '/simulators')
-from lbp.factor_graph import FactorGraph
-from lbp.factors import Factor
+from lbp import LBP
 from fires.LatticeForest import LatticeForest
-
 from Observe import get_forest_observation, tree_observation_probability
 
-# function to map a node in the graphical model to a numeric index
-def node2index(dims, idx, time):
-    return time*np.prod(dims) + idx
+
+# # function to map a node in the graphical model to a numeric index
+# def node2index(dims, idx, t):
+#     return t*np.prod(dims) + idx
+#
+#
+# class LBP:
+#     """
+#     Implementation of loopy belief propagation for the LatticeForest simulator.
+#     """
+#     def __init__(self, simulation, observation, prior, iteration_limit=1, horizon=3):
+#         self.healthy = 0
+#         self.on_fire = 1
+#         self.burnt = 2
+#
+#         self.G = FactorGraph(numVar=0, numFactor=0)
+#         self.factorIdx = 0
+#         self.time = 0
+#         self.observation_history = []
+#         self.prior = prior
+#
+#         self.add_new_layer(simulation, observation, self.prior)
+#         self.observation_history += [observation]
+#
+#         self.iteration_limit = iteration_limit
+#         self.horizon = horizon
+#
+#     def add_new_layer(self, simulation, observation, prior=None):
+#         """
+#         Adds a new time layer to the graph corresponding to the new measurement y. If prior is given (numpy array) and
+#         self.time = 0 then the layer also includes factors corresponding to the priors.
+#
+#         First, this function builds the nodes for each new Tree element at the current time. Then, it adds factors for
+#         measurements. If self.time >0 then factors are added for the dynamics.
+#         """
+#         # number of factors and variables
+#         num_nodes = np.prod(simulation.dims)
+#         if self.time == 0 and prior is None:
+#             node_factors = 0
+#         else:
+#             node_factors = np.prod(simulation.dims)
+#
+#         obs_factors = np.prod(simulation.dims)  # factors for observations
+#         factor_count = node_factors + obs_factors
+#
+#         self.G.varName += [[] for _ in range(num_nodes)]
+#         self.G.domain += [element.state_space for element in simulation.group.values()]
+#         self.G.varToFactor += [[] for _ in range(num_nodes)]
+#         self.G.factorToVar += [[] for _ in range(factor_count)]
+#
+#         # add factors for each Tree element
+#         for element in simulation.group.values():
+#             # create variable name and index
+#             row, col = element.position
+#             var_idx = node2index(simulation.dims, element.numeric_id, self.time)
+#             xname = 'x' + str(row) + str(col) + str(self.time)
+#
+#             self.G.var.append(var_idx)
+#             self.G.varName[var_idx] = xname
+#
+#             # scope and cardinality for measurement factor
+#             scope = [var_idx]
+#             card = [len(element.state_space)]
+#             val = np.zeros(card)
+#
+#             # create factor for measurement
+#             for x in element.state_space:
+#                 val[x] = tree_observation_probability(x, observation[row, col])
+#
+#             name = 'g_%i%i%i' % (row, col, self.time)
+#             self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
+#
+#             # varToFactor[i] is list of indices of factors corresponding to variable i
+#             # factorToVar[j] is list of variables connected to factor j
+#             self.G.varToFactor[var_idx] += [self.factorIdx]
+#             self.G.factorToVar[self.factorIdx] += [var_idx]
+#             self.factorIdx += 1
+#
+#             if self.time == 0 and prior is not None:
+#                 # create prior factors
+#                 scope = [var_idx]
+#                 card = [len(element.state_space)]
+#                 val = prior[row, col]
+#
+#                 name = 'b_%i%i%i' % (row, col, self.time)
+#                 self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
+#
+#                 self.G.varToFactor[var_idx] += [self.factorIdx]
+#                 self.G.factorToVar[self.factorIdx] += [var_idx]
+#                 self.factorIdx += 1
+#
+#             # for this case, include factors between time slices
+#             elif self.time > 0:
+#                 # create factor between t-1 and t
+#                 scope = [node2index(simulation.dims, element.numeric_id, self.time-1)]
+#                 for n in element.neighbors:
+#                     scope += [node2index(simulation.dims, simulation.group[n].numeric_id, self.time-1)]
+#                 scope += [var_idx]
+#
+#                 # use dynamics for factor
+#                 card = [len(element.state_space) for _ in range(len(scope))]
+#                 val = np.zeros(card)
+#                 iterate = [element.state_space for _ in range(len(scope))]
+#                 for combo in itertools.product(*iterate):
+#                     x_tm1 = combo[0]
+#                     f = combo[1:-1].count(self.on_fire)
+#                     x_t = combo[-1]
+#
+#                     val[combo] = element.dynamics((x_tm1, f, x_t), (0, 0))
+#
+#                 name = 'f_%i%i%i' % (row, col, self.time-1)
+#                 self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
+#
+#                 for idx in scope:
+#                     # connect this new factor to each variable's list
+#                     self.G.varToFactor[idx] += [self.factorIdx]
+#
+#                 # connect this variable to factor's list
+#                 self.G.factorToVar[self.factorIdx] += scope
+#                 self.factorIdx += 1
+#
+#         self.time += 1
+#
+#     def filter(self, simulation, observation):
+#         """
+#         Method to build the graphical model and generate the belief.
+#         """
+#         tic = time.clock()
+#         self.observation_history += [observation]
+#
+#         # case where graphical model has not grown too large
+#         if 1 <= self.time < self.horizon:
+#             self.add_new_layer(simulation, observation)
+#             status = self.G.runParallelLoopyBP(self.iteration_limit)
+#
+#         # graphical model is too large, rebuild
+#         else:
+#             # get prior at time 1
+#             prior = self.query_belief(simulation, 1)
+#
+#             obs_hist = self.observation_history[-self.horizon:]
+#
+#             # rebuild model
+#             self.G = FactorGraph(numVar=0, numFactor=0)
+#             self.factorIdx = 0
+#             self.time = 0
+#
+#             # create first layer with measurement and priors
+#             self.add_new_layer(simulation, obs_hist[0], prior)
+#
+#             # create remaining layers with measurements
+#             for i in range(1, self.horizon):
+#                 self.add_new_layer(simulation, obs_hist[i])
+#
+#             status = self.G.runParallelLoopyBP(self.iteration_limit)
+#
+#         toc = time.clock()
+#         return self.query_belief(simulation, self.time-1), status, toc-tic
+#
+#     def query_belief(self, simulation, time):
+#         """
+#         Method to get belief from graphical model. Returns a numpy array where (row, col) corresponds to a 1D array
+#         containing the state probabilities for the Tree positioned at (row, col).
+#         """
+#         belief = np.zeros((simulation.dims[0], simulation.dims[1], 3))
+#
+#         for element in simulation.group.values():
+#             var_idx = node2index(simulation.dims, element.numeric_id, time)
+#             row, col = element.position
+#             b = self.G.estimateMarginalProbability(var_idx)
+#             belief[row, col] = b
+#
+#         return belief
+
+def obs_model(tree, state_value, obs_value):
+    return tree_observation_probability(state_value, obs_value)
 
 
-class LBP:
-    """
-    Implementation of loopy belief propagation for the LatticeForest simulator.
-    """
-    def __init__(self, simulation, observation, prior, iteration_limit=1, horizon=3):
-        self.healthy = 0
-        self.on_fire = 1
-        self.burnt = 2
-
-        self.G = FactorGraph(numVar=0, numFactor=0)
-        self.factorIdx = 0
-        self.time = 0
-        self.observation_history = []
-        self.prior = prior
-
-        self.add_new_layer(simulation, observation, self.prior)
-        self.observation_history += [observation]
-
-        self.iteration_limit = iteration_limit
-        self.horizon = horizon
-
-    def add_new_layer(self, simulation, observation, prior=None):
-        """
-        Adds a new time layer to the graph corresponding to the new measurement y. If prior is given (numpy array) and
-        self.time = 0 then the layer also includes factors corresponding to the priors.
-
-        First, this function builds the nodes for each new Tree element at the current time. Then, it adds factors for
-        measurements. If self.time >0 then factors are added for the dynamics.
-        """
-        # number of factors and variables
-        num_nodes = np.prod(simulation.dims)
-        if self.time == 0 and prior is None:
-            node_factors = 0
-        else:
-            node_factors = np.prod(simulation.dims)
-
-        obs_factors = np.prod(simulation.dims)  # factors for observations
-        factor_count = node_factors + obs_factors
-
-        self.G.varName += [[] for _ in range(num_nodes)]
-        self.G.domain += [element.state_space for element in simulation.group.values()]
-        self.G.varToFactor += [[] for _ in range(num_nodes)]
-        self.G.factorToVar += [[] for _ in range(factor_count)]
-
-        # add factors for each Tree element
-        for element in simulation.group.values():
-            # create variable name and index
-            row, col = element.position
-            var_idx = node2index(simulation.dims, element.numeric_id, self.time)
-            xname = 'x' + str(row) + str(col) + str(self.time)
-
-            self.G.var.append(var_idx)
-            self.G.varName[var_idx] = xname
-
-            # scope and cardinality for measurement factor
-            scope = [var_idx]
-            card = [len(element.state_space)]
-            val = np.zeros(card)
-
-            # create factor for measurement
-            for x in element.state_space:
-                val[x] = tree_observation_probability(x, observation[row, col])
-
-            name = 'g_%i%i%i' % (row, col, self.time)
-            self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
-
-            # varToFactor[i] is list of indices of factors corresponding to variable i
-            # factorToVar[j] is list of variables connected to factor j
-            self.G.varToFactor[var_idx] += [self.factorIdx]
-            self.G.factorToVar[self.factorIdx] += [var_idx]
-            self.factorIdx += 1
-
-            if self.time == 0 and prior is not None:
-                # create prior factors
-                scope = [var_idx]
-                card = [len(element.state_space)]
-                val = prior[row, col]
-
-                name = 'b_%i%i%i' % (row, col, self.time)
-                self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
-
-                self.G.varToFactor[var_idx] += [self.factorIdx]
-                self.G.factorToVar[self.factorIdx] += [var_idx]
-                self.factorIdx += 1
-
-            # for this case, include factors between time slices
-            elif self.time > 0:
-                # create factor between t-1 and t
-                scope = [node2index(simulation.dims, element.numeric_id, self.time-1)]
-                for n in element.neighbors:
-                    scope += [node2index(simulation.dims, simulation.group[n].numeric_id, self.time-1)]
-                scope += [var_idx]
-
-                # use dynamics for factor
-                card = [len(element.state_space) for _ in range(len(scope))]
-                val = np.zeros(card)
-                iterate = [element.state_space for _ in range(len(scope))]
-                for combo in itertools.product(*iterate):
-                    x_tm1 = combo[0]
-                    f = combo[1:-1].count(self.on_fire)
-                    x_t = combo[-1]
-
-                    val[combo] = element.dynamics((x_tm1, f, x_t), (0, 0))
-
-                name = 'f_%i%i%i' % (row, col, self.time-1)
-                self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
-
-                for idx in scope:
-                    # connect this new factor to each variable's list
-                    self.G.varToFactor[idx] += [self.factorIdx]
-
-                # connect this variable to factor's list
-                self.G.factorToVar[self.factorIdx] += scope
-                self.factorIdx += 1
-
-        self.time += 1
-
-    def filter(self, simulation, observation):
-        """
-        Method to build the graphical model and generate the belief.
-        """
-        tic = time.clock()
-        self.observation_history += [observation]
-
-        # case where graphical model has not grown too large
-        if 1 <= self.time < self.horizon:
-            self.add_new_layer(simulation, observation)
-            status = self.G.runParallelLoopyBP(self.iteration_limit)
-
-        # graphical model is too large, rebuild
-        else:
-            # get prior at time 1
-            prior = self.query_belief(simulation, 1)
-
-            obs_hist = self.observation_history[-self.horizon:]
-
-            # rebuild model
-            self.G = FactorGraph(numVar=0, numFactor=0)
-            self.factorIdx = 0
-            self.time = 0
-
-            # create first layer with measurement and priors
-            self.add_new_layer(simulation, obs_hist[0], prior)
-
-            # create remaining layers with measurements
-            for i in range(1, self.horizon):
-                self.add_new_layer(simulation, obs_hist[i])
-
-            status = self.G.runParallelLoopyBP(self.iteration_limit)
-
-        toc = time.clock()
-        return self.query_belief(simulation, self.time-1), status, toc-tic
-
-    def query_belief(self, simulation, time):
-        """
-        Method to get belief from graphical model. Returns a numpy array where (row, col) corresponds to a 1D array
-        containing the state probabilities for the Tree positioned at (row, col).
-        """
-        belief = np.zeros((simulation.dims[0], simulation.dims[1], 3))
-
-        for element in simulation.group.values():
-            var_idx = node2index(simulation.dims, element.numeric_id, time)
-            row, col = element.position
-            b = self.G.estimateMarginalProbability(var_idx)
-            belief[row, col] = b
-
-        return belief
+def trans_model(tree, combo):
+    x_tm1 = combo[0]
+    f = combo[1:-1].count(tree.on_fire)
+    x_t = combo[-1]
+    return tree.dynamics((x_tm1, f, x_t))
 
 
 def run_simulation(sim_obj, iteration_limit, horizon):
@@ -193,7 +202,6 @@ def run_simulation(sim_obj, iteration_limit, horizon):
     Function to run a single simulation of the LatticeForest simulator using loopy belief propagation.
     """
     num_trees = np.prod(sim_obj.dims)
-    control = defaultdict(lambda: (0, 0))
 
     # initial belief - exact state
     belief = np.zeros((sim_obj.dims[0], sim_obj.dims[1], 3))
@@ -207,7 +215,8 @@ def run_simulation(sim_obj, iteration_limit, horizon):
     belief = belief / belief.sum(axis=2, keepdims=True)
 
     # instantiate filter
-    robot = LBP(sim_obj, sim_obj.dense_state(), belief, iteration_limit=iteration_limit, horizon=horizon)
+    robot = LBP(sim_obj, sim_obj.dense_state(), obs_model, trans_model, belief,
+                iteration_limit=iteration_limit, horizon=horizon)
 
     observation_acc = []
     filter_acc = []
@@ -216,7 +225,7 @@ def run_simulation(sim_obj, iteration_limit, horizon):
     # run until forest fire terminates
     while not sim_obj.end:
         # update simulator and get state
-        sim_obj.update(control)
+        sim_obj.update()
         state = sim_obj.dense_state()
 
         # get observation and observation accuracy
@@ -224,8 +233,11 @@ def run_simulation(sim_obj, iteration_limit, horizon):
         obs_acc = np.sum(obs == state)/num_trees
 
         # run filter and get belief
-        belief, status, timing = robot.filter(sim_obj, obs)
-        estimate = np.argmax(belief, axis=2)
+        belief, status, timing = robot.filter(sim_obj, obs, obs_model, trans_model)
+        # estimate = np.argmax(belief, axis=2)
+        estimate = np.zeros_like(state)
+        for key in belief.keys():
+            estimate[key] = np.argmax(belief[key])
         f_acc = np.sum(estimate == state)/num_trees
         update_time.append(timing)
 
@@ -248,8 +260,8 @@ def benchmark(arguments):
     total_sims = 1
 
     if len(arguments) > 1:
-        dimension = int(sys.argv[1][1:])
-        Kmax = int(sys.argv[2][1:])
+        dimension = int(arguments[1][1:])
+        Kmax = int(arguments[2][1:])
 
     print('[LBP] dimension = %d, Kmax = %d' % (dimension, Kmax))
     print('running for %d simulation(s)' % total_sims)
--- lbpWestAfrica.py
+++ lbpWestAfrica.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
 from datetime import datetime
-import itertools
 import numpy as np
 import os
 import pickle
@@ -8,183 +6,20 @@ import sys
 import time
 
 sys.path.append(os.getcwd() + '/simulators')
-from lbp.factor_graph import FactorGraph
-from lbp.factors import Factor
+from lbp import LBP
 from epidemics.WestAfrica import WestAfrica
-
 from Observe import get_ebola_observation, region_observation_probability
 
-# function to map a node in the graphical model to a numerical index
-def node2index(dims, idx, time):
-    return time*np.prod(dims) + idx
+
+def obs_model(region, state_value, obs_value):
+    return region_observation_probability(state_value, obs_value)
 
 
-class LBP:
-    """
-    Implementation of loopy belief propagation for the WestAfrica simulator.
-    """
-    def __init__(self, simulation, observation, prior, iteration_limit=1, horizon=3):
-        self.healthy = 0
-        self.infected = 1
-        self.immune = 2
-
-        self.G = FactorGraph(numVar=0, numFactor=0)
-        self.factorIdx = 0
-        self.time = 0
-
-        self.observation_history = []
-        self.prior = prior
-
-        self.add_new_layer(simulation, observation, self.prior)
-        self.observation_history += [observation]
-
-        self.iteration_limit = iteration_limit
-        self.horizon = horizon
-
-    def add_new_layer(self, simulation, observation, prior=None):
-        """
-        Adds a new time layer to the graph corresponding to the new measurement y. If prior is given (numpy array) and
-        self.time = 0 then the layer also includes factors corresponding to the priors.
-
-        First, this function builds the nodes for each new Region element at the current time. Then, it adds factors for
-        measurements. If self.time > 0 then factors are added for the dynamics.
-        """
-        # number of factors and variables
-        num_nodes = np.prod(simulation.dims)
-        if self.time == 0 and prior is None:
-            node_factors = 0
-        else:
-            node_factors = np.prod(simulation.dims)
-        obs_factors = np.prod(simulation.dims)  # factors for observations
-        factor_count = node_factors + obs_factors
-
-        self.G.varName += [[] for _ in range(num_nodes)]
-        self.G.domain += [element.state_space for element in simulation.group.values()]
-        self.G.varToFactor += [[] for _ in range(num_nodes)]
-        self.G.factorToVar += [[] for _ in range(factor_count)]
-
-        # add variables and factors for each Region element
-        for key, element in simulation.group.items():
-            # create variable name and index
-            var_idx = node2index(simulation.dims, element.numeric_id, self.time)
-            xname = 'x' + str(element.numeric_id) + str(self.time)
-
-            self.G.var.append(var_idx)
-            self.G.varName[var_idx] = xname
-
-            # scope and cardinality for measurement factor
-            scope = [var_idx]
-            card = [len(element.state_space)]
-            val = np.zeros(card)
-
-            # create factor for measurement
-            for x in element.state_space:
-                val[x] = region_observation_probability(x, observation[key])
-
-            name = 'g_%i%i' % (element.numeric_id, self.time)
-            self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
-
-            # varToFactor[i] is list of indices of factors corresponding to variable i
-            # factorToVar[j] is list of variables connected to factor j
-            self.G.varToFactor[var_idx] += [self.factorIdx]
-            self.G.factorToVar[self.factorIdx] += [var_idx]
-            self.factorIdx += 1
-
-            if self.time == 0 and prior is not None:
-                # create prior factors
-                scope = [var_idx]
-                card = [len(element.state_space)]
-                val = prior[key]
-
-                name = 'b_%i%i' % (element.numeric_id, self.time)
-                self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
-
-                self.G.varToFactor[var_idx] += [self.factorIdx]
-                self.G.factorToVar[self.factorIdx] += [var_idx]
-                self.factorIdx += 1
-
-            # for this case, include factors between time slices
-            elif self.time > 0:
-                # create factor between t-1 and t
-                scope = [node2index(simulation.dims, element.numeric_id, self.time-1)]
-                for n in element.neighbors:
-                    scope += [node2index(simulation.dims, simulation.group[n].numeric_id, self.time-1)]
-                scope += [var_idx]
-
-                # use dynamics for factor
-                card = [len(element.state_space) for _ in range(len(scope))]
-                val = np.zeros(card)
-                iterate = [element.state_space for _ in range(len(scope))]
-                for combo in itertools.product(*iterate):
-                    x_tm1 = combo[0]
-                    f = combo[1:-1].count(self.infected)
-                    x_t = combo[-1]
-
-                    val[combo] = element.dynamics((x_tm1, f, x_t), (0, 0))
-
-                name = 'f_%i%i' % (element.numeric_id, self.time-1)
-                self.G.factors.append(Factor(scope=scope, card=card, val=val, name=name))
-
-                for idx in scope:
-                    # connect this new factor to each variable's list
-                    self.G.varToFactor[idx] += [self.factorIdx]
-
-                # connect this variable to factor's list
-                self.G.factorToVar[self.factorIdx] += scope
-                self.factorIdx += 1
-
-        self.time += 1
-
-    def filter(self, simulation, observation):
-        """
-        Method to build the graphical model and generate the belief.
-        """
-        tic = time.clock()
-        self.observation_history += [observation]
-
-        # case where graphical model has not grown too large
-        if 1 <= self.time < self.horizon:
-            # add layer for measurement
-            self.add_new_layer(simulation, observation)
-            status = self.G.runParallelLoopyBP(self.iteration_limit)
-
-        # graphical model is too large, rebuild
-        else:
-            # get prior at time 1
-            prior = self.query_belief(simulation, 1)
-
-            obs_hist = self.observation_history[-self.horizon:]
-
-            # create new graph
-            self.G = FactorGraph(numVar=0, numFactor=0)
-            self.factorIdx = 0
-            self.time = 0
-
-            # create first layer with measurement and priors
-            self.add_new_layer(simulation, obs_hist[0], prior)
-
-            # create remaining layers with measurements
-            for i in range(1, self.horizon):
-                self.add_new_layer(simulation, obs_hist[i])
-
-            status = self.G.runParallelLoopyBP(self.iteration_limit)
-
-        toc = time.clock()
-        return self.query_belief(simulation, self.time-1), status, toc-tic
-
-    def query_belief(self, simulation, time):
-        """
-        Method to get belief from graphical model. Returns a dictionary where each key is the name of a Region and
-        values correspond to a list with a probability for each Region state.
-        """
-        belief = {}
-
-        for key in simulation.group.keys():
-            var_idx = node2index(simulation.dims, simulation.group[key].numeric_id, time)
-            b = self.G.estimateMarginalProbability(var_idx)
-            belief[key] = b
-
-        return belief
+def trans_model(region, combo):
+    x_tm1 = combo[0]
+    f = combo[1:-1].count(region.infected)
+    x_t = combo[-1]
+    return region.dynamics((x_tm1, f, x_t))
 
 
 def run_simulation(sim_obj, iteration_limit, horizon):
@@ -192,7 +27,6 @@ def run_simulation(sim_obj, iteration_limit, horizon):
     Function to run a single simulation of the WestAfrica simulator using loopy belief propagation.
     """
     num_regions = np.prod(sim_obj.dims)
-    control = defaultdict(lambda: (0, 0))  # no control
 
     # initial belief - exact state
     belief = {}
@@ -202,7 +36,8 @@ def run_simulation(sim_obj, iteration_limit, horizon):
         belief[key][state[key]] = 1
 
     # instantiate filter
-    robot = LBP(sim_obj, sim_obj.dense_state(), belief, iteration_limit=iteration_limit, horizon=horizon)
+    robot = LBP(sim_obj, sim_obj.dense_state(), obs_model, trans_model, belief,
+                iteration_limit=iteration_limit, horizon=horizon)
 
     observation_acc = []
     filter_acc = []
@@ -211,7 +46,7 @@ def run_simulation(sim_obj, iteration_limit, horizon):
     # run for only 75 iterations as process is non self-terminating
     for _ in range(75):
         # update simulator and get state
-        sim_obj.update(control)
+        sim_obj.update()
         state = sim_obj.dense_state()
 
         # get observation and observation accuracy
@@ -219,7 +54,7 @@ def run_simulation(sim_obj, iteration_limit, horizon):
         obs_acc = [obs[name] == state[name] for name in state.keys()].count(True)/num_regions
 
         # run filter and get belief
-        belief, status, timing = robot.filter(sim_obj, obs)
+        belief, status, timing = robot.filter(sim_obj, obs, obs_model, trans_model)
         estimate = {name: np.argmax(belief[name]) for name in state.keys()}
         f_acc = [estimate[name] == state[name] for name in state.keys()].count(True)/num_regions
         update_time.append(timing)
@@ -242,8 +77,8 @@ def benchmark(arguments):
     H = 3  # horizon parameter for LBP
     total_sims = 100
 
-    if len(sys.argv) > 1:
-        Kmax = int(sys.argv[1][1:])
+    if len(arguments) > 1:
+        Kmax = int(arguments[1][1:])
 
     print('[LBP] Kmax = %d' % Kmax)
     print('running for %d simulation(s)' % total_sims)
